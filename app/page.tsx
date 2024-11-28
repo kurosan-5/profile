a919c2b2e8338f251491a52e871833433471df6b
kurosan5 <kurowassan55555@gmail.com>
2024-11-28 16:53:21 +0900

    概要

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,224 @@
-import Image from "next/image";
+'use client'
+import { Grid2, Typography } from '@mui/material';
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const message = "Weelcome";
+  const [welcome, setWelcome] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  useEffect(() => {
+    let currentIndex = 0;
+    const interval = setInterval(() => {
+      setWelcome((prev) => prev + message[currentIndex]);
+      currentIndex++;
+
+      if (currentIndex >= message.length - 1) {
+        clearInterval(interval);
+      }
+    }, 800); // 0.5秒ごとに次の文字を追加
+
+    return () => clearInterval(interval); // クリーンアップ
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Poppins:wght@400&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Grid2
+        container
+        direction='column'
+      >
+        <Grid2
+          sx={{
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontFamily: "'Playfair Display', serif",
+          }}
+        >
+          <Typography
+            variant='h3'
+            component='h3'
+            sx={{
+              fontFamily: "'Montserrat', sans-serif",
+              fontSize: '4rem'
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            {welcome}
+          </Typography>
+        </Grid2>
+
+
+        {/* ここから名前のところ */}
+        <Grid2
+          sx={{
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+
+          }}
+        >
+          <Image
+            src='/profile.jpg'
+            alt='profile image'
+            width={300}
+            height={300}
+            style={{
+              borderRadius: 20,
+              marginRight: "10%",
+
+            }}
+          />
+          <Grid2
+            container
+            direction='column'
+            rowSpacing={1}
           >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+            <Grid2
+              container
+              direction='row'
+            >
+              <Grid2>
+                <Typography
+                  variant='h4'
+                  component='h4'
+                  sx={{
+                    fontFamily: 'var(--font-geist-mono)',
+                  }}
+                >
+                  金森大直
+                </Typography>
+              </Grid2>
+              <Grid2>
+                <Typography
+                  variant='body1'
+                  component='p'
+                  sx={{
+                    marginLeft: 3,
+                    marginTop: 2,
+                    color: "text.secondary"
+                  }}
+                >
+                  Kanamori Hirosugu
+                </Typography>
+              </Grid2>
+            </Grid2>
+            <Grid2>
+              <Typography
+                variant='h5'
+                component='h5'
+              >
+                大阪大学基礎工学部化学応用科学科
+              </Typography>
+            </Grid2>
+            <Grid2>
+              <Typography
+                variant='body1'
+                component='p'
+              >
+                志望 - Webアプリエンジニア
+              </Typography>
+            </Grid2>
+            <Grid2>
+              <Typography
+                variant='body1'
+                component='p'
+              >
+                趣味 - 旅行・サイクリング
+              </Typography>
+            </Grid2>
+            <Grid2>
+              <Typography
+                variant='body1'
+                component='p'
+              >
+                好きな食べ物 - お寿司・焼きとり
+              </Typography>
+            </Grid2>
+          </Grid2>
+        </Grid2>
+
+
+
+        {/* ここから技術スタック */}
+        <Grid2
+          sx={{
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <Grid2
+            container
+            direction='column'
+            rowSpacing={1}
+          >
+            <Grid2>
+              <Typography
+                variant='h5'
+                component='h5'
+              >
+                開発経験のある言語・ツール
+              </Typography>
+            </Grid2>
+            <Grid2>
+              <Typography
+                variant="body1"
+                component='p'
+              >
+                フレームワーク - Next.js・Laravel
+              </Typography>
+            </Grid2>
+            <Grid2>
+              <Typography
+                variant="body1"
+                component='p'
+              >
+                フロントエンド - React・Vue.js・Typescript
+              </Typography>
+            </Grid2>
+            <Grid2>
+              <Typography
+                variant="body1"
+                component='p'
+              >
+                バックエンド - Node.js + Express, Python
+              </Typography>
+            </Grid2>
+            <Grid2>
+              <Typography
+                variant="body1"
+                component='p'
+              >
+                データベース - MySQL・PostgreSQL・NoSQL
+              </Typography>
+            </Grid2>
+            <Grid2>
+              <Typography
+                variant="body1"
+                component='p'
+              >
+                バージョン管理 - GitHub
+              </Typography>
+            </Grid2>
+          </Grid2>
+
+        </Grid2>
+
+
+
+      </Grid2>
+
+    </>
+
   );
 }
